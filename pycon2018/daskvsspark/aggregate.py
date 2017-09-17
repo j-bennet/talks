869a494a0adf1317a7b369ce786c0103eb4b6281
Irina Truong <i.chernyavska@gmail.com>
2017-09-16 19:43:38 -0700

    Moved transformation into sql.

--- pycon2018/daskvsspark/aggregate.py
+++ pycon2018/daskvsspark/aggregate.py
@@ -5,7 +5,6 @@ import shutil
 from collections import Counter
 
 from pyspark.sql.types import StringType, IntegerType, MapType
-from pyspark.sql.functions import udf, lit
 
 from context import initialize, INPUT_PATH, OUTPUT_PATH
 
@@ -13,32 +12,12 @@ if os.environ.get('TZ', '') != 'UTC':
     raise Exception('Please set TZ=UTC to run this.')
 
 
-def aggregate(df):
-    """Group data by customer, url, and 1 hour bucket."""
-    df.createOrReplaceTempView("df")
-    agg = sqlContext.sql("""
-    select 
-        customer,
-        url,
-        window(ts, '1 hour').start as ts,
-        count(*) as page_views,
-        count(distinct(session_id)) as visitors,
-        collect_list(referrer) as referrers
-    from df
-    group by
-        customer,
-        url,
-        window(ts, '1 hour').start
-    """)
-    return agg
-
-
-def make_id(customer, url, ts):
+def format_id(customer, url, ts):
     """Create a unique id for the aggregated record."""
     return "{}|{}|{:%Y-%m-%dT%H:%M:%S}".format(url, customer, ts)
 
 
-def make_metrics(visitors, page_views):
+def format_metrics(visitors, page_views):
     """Create a dict of metrics."""
     return {
         "page_views": page_views,
@@ -46,35 +25,52 @@ def make_metrics(visitors, page_views):
     }
 
 
-def make_referrers(referrers):
+def format_referrers(referrers):
     """Create a dict of referrer counts."""
     counter = Counter(referrers)
     return dict(counter)
 
 
-def transform(df):
-    """Transform data into complex json representation."""
-    udf_id = udf(
-        lambda *args: make_id(*args),
-        StringType())
+def load_sql_user_functions(sqlContext):
+    """Load our custom UDAFs into a sql context."""
+    sqlContext.udf.register('format_id',
+                            format_id,
+                            StringType())
+    sqlContext.udf.register('format_referrers',
+                            format_referrers,
+                            MapType(StringType(), IntegerType()))
+    sqlContext.udf.register('format_referrers',
+                            format_referrers,
+                            MapType(StringType(), IntegerType()))
 
-    udf_metrics = udf(
-        lambda *args: make_metrics(*args),
-        MapType(StringType(), IntegerType()))
 
-    udf_refs = udf(
-        lambda *args: make_referrers(*args),
-        MapType(StringType(), IntegerType()))
-
-    df = (df.withColumn('_id', udf_id('customer', 'url', 'ts'))
-            .withColumn('_index', lit('events'))
-            .withColumn('freq', lit('1hour'))
-            .withColumn('metrics', udf_metrics('visitors', 'page_views'))
-            .withColumn('referrers', udf_refs('referrers'))
-            .drop('page_views')
-            .drop('visitors')
-            .drop('referrers'))
-    return df
+def aggregate(df):
+    """Group data by customer, url, and 1 hour bucket."""
+    df.createOrReplaceTempView("df")
+    agg = sqlContext.sql("""
+    select 
+      format_id(customer, url, ts) as _id,
+      customer,
+      url,
+      ts,
+      format_metrics(page_views, visitors) as metrics,
+      format_referrers(referrers) as referrers
+    from (
+        select 
+            customer,
+            url,
+            window(ts, '1 hour').start as ts,
+            count(*) as page_views,
+            count(distinct(session_id)) as visitors,
+            collect_list(referrer) as referrers
+        from df
+        group by
+            customer,
+            url,
+            window(ts, '1 hour').start
+    )
+    """)
+    return agg
 
 
 def save_json(df, path):
@@ -87,7 +83,7 @@ def save_json(df, path):
 
 if __name__ == '__main__':
     sc, sqlContext = initialize()
+    load_sql_user_functions(sqlContext)
     df = sqlContext.read.parquet(INPUT_PATH)
     agg = aggregate(df)
-    prep = transform(agg)
-    save_json(prep, OUTPUT_PATH)
+    save_json(agg, OUTPUT_PATH)
--- pycon2018/daskvsspark/show.py
+++ pycon2018/daskvsspark/show.py
@@ -19,12 +19,15 @@ if __name__ == '__main__':
     json_files = glob.glob1(OUTPUT_PATH, '*.json')
     file_name = os.path.join(OUTPUT_PATH, json_files[0])
 
-    collected = 0
+    collected, total = 0, 0
     with open(file_name, 'r') as f:
         for line in f:
-            data = json.loads(line)
-            pprint(data)
-            print("")
-            collected += 1
-            if collected == requested:
-                break
+            if collected < requested:
+                data = json.loads(line)
+                pprint(data)
+                print("")
+                collected += 1
+            total += 1
+
+    print('-' * 20)
+    print('Total: {}'.format(total))
