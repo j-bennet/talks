812a079d377f8f43932797033c291e56751b6024
Irina Truong <i.chernyavska@gmail.com>
2018-03-25 19:14:52 -0700

    Modified showing script to go through all files.

--- 2018/daskvsspark/show.py
+++ 2018/daskvsspark/show.py
@@ -16,17 +16,19 @@ if __name__ == '__main__':
     json_path = sys.argv[1]
     requested = int(sys.argv[2])
     json_files = glob.glob1(json_path, '*.json')
-    file_name = os.path.join(json_path, json_files[0])
 
     collected, total = 0, 0
-    with open(file_name, 'r') as f:
-        for line in f:
-            if collected < requested:
-                data = json.loads(line)
-                pprint(data)
-                print("")
-                collected += 1
-            total += 1
+    for file_name in json_files:
+        full_name = os.path.join(json_path, file_name)
+        with open(full_name, 'r') as f:
+            for line in f:
+                if collected < requested:
+                    data = json.loads(line)
+                    pprint(data)
+                    print("")
+                    collected += 1
+                total += 1
 
     print('-' * 20)
-    print('Total: {}'.format(total))
+    print('Total files: {}'.format(len(json_files)))
+    print('Total lines: {}'.format(total))
