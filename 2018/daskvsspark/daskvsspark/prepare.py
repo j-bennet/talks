714a23f67bcc174cdf9eb4e0f65f39119a60bd80
Irina Truong <i.chernyavska@gmail.com>
2018-04-07 17:39:33 -0700

    Improve data gen script.

--- 2018/daskvsspark/daskvsspark/prepare.py
+++ 2018/daskvsspark/daskvsspark/prepare.py
@@ -2,6 +2,8 @@
 # prepare.py
 import argparse
 import datetime as dt
+import math
+import itertools
 import os
 import random
 import sys
@@ -15,15 +17,15 @@ from daskvsspark.schema import MY_SCHEMA, PARTITION_FIELDS
 DATE = dt.datetime(2017, 9, 17)
 
 
-def generate_row():
+def generate_row(total_articles, session_ids):
     """Create page view event."""
     # tuple fields:
     # customer, url, referrer, session_id, ts, year, month, day, hour
     minute = random.randint(0, 59)
     hour = random.randint(0, 23)
-    article_number = random.randint(1, 20)
+    article_number = random.randint(1, total_articles)
     referrer = random.choice(['http://google.com/', 'http://bing.com/', 'http://facebook.com/'])
-    session_id = random.choice(['xxx', 'yyy'])
+    session_id = random.choice(session_ids)
     return (
         'a.com',
         'http://a.com/articles/{}'.format(article_number),
@@ -54,8 +56,12 @@ def generate_rows(sc, records, records_per_file):
         parts_per_hour,
         records_per_file,
         actual_records_per_file))
+
+    total_articles = math.ceil(math.pow(records, 1.0/3))
+    session_ids = [''.join(t) for t in list(itertools.permutations(list('abcdefg'), 3))]
     data = (sc.parallelize([], parts_per_hour)
-              .mapPartitions(lambda rs: (generate_row() for _ in range(part_size))))
+              .mapPartitions(lambda rs: (generate_row(total_articles, session_ids)
+                                         for _ in range(part_size))))
     return data
 
 
