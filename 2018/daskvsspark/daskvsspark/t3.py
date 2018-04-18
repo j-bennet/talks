8f778d1393bcd1d295e2ef986a2d2aed4ac6ebd4
Irina Truong <i.chernyavska@gmail.com>
2018-04-17 22:48:36 -0700

    Deleted test files.

--- 2018/daskvsspark/daskvsspark/t3.py
+++
@@ -1,8 +0,0 @@
-import dask.dataframe as dd
-from dask.distributed import Client
-
-
-if __name__ == '__main__':
-    client = Client()
-    df = dd.read_parquet('s3://path/here')
-    print(df.head(3))
