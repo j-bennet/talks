301cf1cbff0843591928ef97f8127444e81bd22e
Irina Truong <i.chernyavska@gmail.com>
2018-05-06 16:20:42 -0700

    Kubernetes stuff works locally.

---
+++ 2018/daskvsspark/daskvsspark/aggregate_dask_kube.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Stop at any error
+set -e
+
+if [ ! -z $1 ]
+then
+    ADDRESS=$1
+else
+    echo "Usage: $0 <SCHEDULER ADDRESS>"
+    exit 1
+fi
+
+if [ ! -z $2 ]
+then
+    COUNT=$2
+else
+    COUNT=100
+fi
+
+if [ ! -z $3 ]
+then
+    NFILES=$3
+else
+    NFILES=24
+fi
+
+
+if [ ! -z $4 ]
+then
+    SCHEDULER=$4
+else
+    SCHEDULER="default"
+fi
+
+cd /assets/code/daskvsspark
+
+python aggregate_dask.py \
+    --input "s3://parsely-public/jbennet/daskvsspark/events" \
+    --output "s3://parsely-public/jbennet/daskvsspark/aggs_dask" \
+    --address $ADDRESS \
+    --count $COUNT \
+    --nfiles $NFILES \
+    --scheduler $SCHEDULER \
+    --verbose
